15e64f4dcc6a24e55c84d54a4294412573edbe4a
Joe <jkjoeeekim@live.com>
2021-03-24 12:59:54 -0700

    Add import_db.sql

--- AA_Questions/import_db.sql
+++ AA_Questions/import_db.sql
@@ -1,34 +1,44 @@
 PRAGMA foreign_keys = ON;
 
+-- DROP DATABASE questions;
+
 CREATE TABLE users (
-    author_id INTEGER PRIMARY KEY
+    author_id INTEGER PRIMARY KEY,
     fname CHARACTER NOT NULL, 
     lname CHARACTER NOT NULL
 );
 
 CREATE TABLE questions (
-    questions_id INTEGER PRIMARY KEY
+    questions_id INTEGER PRIMARY KEY,
     title CHARACTER NOT NULL,
     body CHARACTER NOT NULL,
-    author_id INTEGER,
-
-    FOREIGN_KEY (author_id) REFERENCES users(author_id)
+    au_id INTEGER,
+    FOREIGN KEY (au_id) REFERENCES users(author_id)
 );
 
 CREATE TABLE question_follows (
     question_follows_id INTEGER PRIMARY KEY,
     author_id INTEGER,
     questions_id INTEGER,
-
-    FOREIGN_KEY (author_id) REFERENCES users(author_id),
-    FOREIGN_KEY (questions_id) REFERENCES questions(questions_id)
+    FOREIGN KEY (author_id) REFERENCES users(author_id),
+    FOREIGN KEY (questions_id) REFERENCES questions(questions_id)
 );
-
 
 CREATE TABLE replies (
     replies_id INTEGER PRIMARY KEY,
-    reply  CHARACTER NOT NULL,
-    question_follows_id INTEGER,
-    
-    FOREIGN_KEY (question_follows_id) REFERENCES question_follows(question_follows_id)
+    reply CHARACTER NOT NULL,
+    questions_id INTEGER,
+    parent_id INTEGER,
+    author_id INTEGER,
+    FOREIGN KEY (questions_id) REFERENCES question_follows(questions_id),
+    FOREIGN KEY (parent_id) REFERENCES replies(replies_id),
+    FOREIGN KEY (author_id) REFERENCES question_follows(author_id)
+);
+
+CREATE TABLE question_likes (
+    author_id INTEGER,
+    questions_id INTEGER,
+    likes BOOLEAN,
+    FOREIGN KEY (author_id) REFERENCES question_follows(author_id),
+    FOREIGN KEY (questions_id) REFERENCES question_follows(questions_id)
 );
